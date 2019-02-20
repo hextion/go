eb096a2402617416a5f641b66a9e436b6a0b45ba
hextion <100ishundred@gmail.com>
2019-02-20 22:05:22 +0300

    stringutil reverse

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/hextion/stringutil"
+)
 
 func main() {
-	fmt.Println("hello, world!")
+	fmt.Println(stringutil.Reverse("!dlrow ,olleh"))
 }
